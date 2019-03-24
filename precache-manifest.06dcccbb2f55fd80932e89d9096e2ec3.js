6a9a69eead9b3b22ab523bb6150ec48661d51445
Russ Perry <rperry1886@gmail.com>
2019-03-24 00:24:31 -0400

    Updates

---
+++ precache-manifest.06dcccbb2f55fd80932e89d9096e2ec3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "143eb19aa518302daf32",
+    "url": "/robofriends/static/js/runtime~main.fb41d28d.js"
+  },
+  {
+    "revision": "b411ea064c15da46a06d",
+    "url": "/robofriends/static/js/main.726423f6.chunk.js"
+  },
+  {
+    "revision": "26247f2873d6317e0118",
+    "url": "/robofriends/static/js/2.a94d1e15.chunk.js"
+  },
+  {
+    "revision": "b411ea064c15da46a06d",
+    "url": "/robofriends/static/css/main.93f7b168.chunk.css"
+  },
+  {
+    "revision": "26247f2873d6317e0118",
+    "url": "/robofriends/static/css/2.4c6d77bc.chunk.css"
+  },
+  {
+    "revision": "171920e9eb2eb16c0cb57730bc6cbf70",
+    "url": "/robofriends/index.html"
+  }
+];
